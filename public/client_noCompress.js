5132fdfd0a39a9322bb5e74127f8e556be7e9e27
plissken2013es <plissken2013es@gmail.com>
2016-09-11 15:02:17 +0200

    BSO removed from game

--- public/client_noCompress.js
+++ public/client_noCompress.js
@@ -50,37 +50,6 @@
 // jfxr
 for(_='o.`Sfxr^32@*`#ba*(01]=+=switch(length;case set102=`){return ,e[a2*Math.random()-1;Array(=new a.ynthvar for(	arams),=1=0,&&(function;break;this. ^P(Settings=(a	b=0;24>b;b++)this[String.fromCharCode(97+b)a[b]||0;c<.c=.);c=b+c+e;if(.18>cd=.18/c;b*=d,c*=d,e*=d}}} ^S(_p^P;a,b,c,d,e,f,g,h,i,j,k,l;re=(a=_p;d00/(f*f+.0e00/(g*g+.0f-h*h*h*.,g=-i*i*i*1e-6,a||(k=.5-n/2,l=5e-5*-oh+l*ll>0?-.9:10ij==m?0:(1-m)1-m)*2e4+@},totalRe=(re();d=_p;a=d.b*d.b*1e5,b=d.c*d.c*1e5,c=d.e*d.e*1e5+12,3(a+b+c)/3|0)},sWave=(m,no=_p,p!s||`v,qv#v*.1,r+3e-4#w,ss#s#s*.1,t+1e-4#t,u!s,vx#x,wg,xq||`r,yr#r#r*.2,zq#q`q<0?-0:0Ap?((1-`p)1-`p)*2e4|0)+@:0,Bd,Cj/2,Dk#k*.,Ea,F=a,G/a,H/b,I/c,J=5/(1+`u#u*20).+s);J>.8J=.8J-J;	Q,S,U,W,Y,Z,K=!1,LMNOPRTVX$_4aa@=_.--;)_[0;	=a--;)aa[b	=0;n>;++if(K);if(A&&++X>=AXre()j&&++i>=jjd*=hfg,d*=f,d>ed=e,w>0K=!0)S=d,C>0$D,S*+Math.sin($)*CS|8>SS=8E||(kl,0>k?k=0:k>.5k=.5)++M>F)M++L1:F=b2:F=c}L0:N=M*G1:N+21-M*H)*B2:N-M*I3:NK=!0}xzy,U=0|z,0>U?U=-U:U>3U023)p&&rq*=r,1e-5>q?qe-5:q>.1q=.1)Z=0;	ca=8;ca--;if(T++,T>=ST%=S,3==E))	da=ada--;)aa[dE0:Y=k>T/S?.5:-.51:Y-T/S*22:W=T/S,W=6.28318531W>.5?W-1:WY.27@3954*W+.405284735*W*W0>W?1:-1Y=.225(0>Y?-1:1)*Y*Y-Y)+Y3:Y=aa[Math.abs(@*T/S|0)]}pQ=R,s*=t,0>s?s=0:s>.1s=.1u?(P(Y-R)*s,P*=J):(R=Y,P=0RP,OR-Q,O*-q,Y=Ox_[V%4Y,Y_[(V-U+4)%4],V++ZY}Z*=.125*N*v,m[Z>?@767:-1>=Z?-@768:@767*Z|0}n}}s^S;window.jsfxr=(as._p.Settings(a);b=s.totalRe(cUint84(b+1)/2|0)+44d=2*s.sWave(new Uint16c.buffer,44beUint@c.buffer,0,44);e[0]17914101d+362]16@80727354450944]65655376441007882008]0485789]635706010d,d44;	fg="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz23456789+/",h="data:audio/wav;se64,";d>f;f3i=c[f]<<16|c[f+1]<<8|c[f+2];hg[i>>18]+g[i>>12&63]+g[i>>6&63]+g[63&i]}h};';G=/[-#@^`]/.exec(_);)with(_.split(G))_=join(shift());eval(_)
 
-// sonant-x
-for(_='lfoj+=`3276^-^8@d=@>d?@:d>^7?^7:d,V)}Enew OfunctionN=N(RenQre$=String.fromCharCode(#;b$ak;case Z_waveform],Y$leaseX),K$turn JsetTimeout(I)Jvoid IHat.songrowLQBuffer0,ODe){var Rfor(a=,b=0;sonantx.},Music255>>8&sustaingetGQerorSound(athis.tack,0E	osc]<<8)waveSizeAudio,b`1,b%1e3===0&&-a>cHinstr..prototype.sonantx;!N("use strict";N eJMh.sin(6.283184*aEN fJe)<0?-1:1}N gJa%1-.5}N hb=a%1*4;J2>b?b-1:3-b}N A$turn.00390625*Mh.pow(1.059463094,a-128EN k,dIN(e=OUint8Array*b*2Kf=e.lQgth-2,gf>=0;)if(e[f]=e[f+1]=128,f-=2g,0);IN(d(eE	;Ig		N l,b,d,e,fg=d.fx_delay_time*e>>1,h=d.fx_delay_amt/,i=ARfor(d=,e=0;b-g>i;k=4*i,l=4*(i+gKm=a[l]+[l+1+[k+2]+[k+3@)*h;if[l]=&m,a[l+1]=m,m=a[l+2]+[l+3+[k]+[k+1@)*h,a[l+2]=&m,a[l+3]=m,++i,e`1,e%1e3===0&&-d>cHA	If	;IA	sonantx={};a=4410b=2,c=33,d=null,i=[e,f,g,h];=NmixBuf=a,=a.lQgth/b/2},WaveRd,e,f,g,h,i,k,a=mixBuf,c=,l=c*b*2;for(h=l-8,i=h-36,g#82,73,77&h,h,h>>16&,h>>24&,87,65,86,69,102,109,116,32,16,1,2,68,172,16,177,2,4,16,1097,116,97,&i,i,i>>16&,i>>24&Kd=0;l>d;for(f="",e=0;256>e&&l>d;++e,d`2)k=4*[d]+[d+1@Kk=@>k?@:k>^7?^7:k,f+#&k,k);g`f}Jg},Ra=Wave(Kb=O("da:audio/wav;base64,"+btoa));Jb.p$load="none",b.load(Kb},Renull===d&&(d=OContext);f=mixBuf,g=,h=d.c$e(b,,aKi=h.ChannelDa(0KA=h.ChannelDa(1Kk=lg>k;d=4*(f[4*k]+(f[4*k+1@);if(Vi[k]=d/^8,d=4*(f[4*k+2]+(f[4*k+3@KVA[k]=d/^8,k`1l	IN(e(hE	;Il	,=N,binstr=a,=b||5605,_j=i[a.jY1=i[a.1Y2=i[a.2Y=a.Qv_tack,=a.Qv_,X=a.Qv_X,panF$q=Mh.pow(2,a.fx_pan_f$q-8)/,jF$q=Mh.pow(2,a.j_f$q-8)/},gQRb,c,dfor(g=(,0Kh=i=A(b+12*(1_oct-8)+1_det)*(1+8e-4*1_detuneKk=A(b+12*(2_oct-8)+2_det)*(1+8e-4*2_detuneKl=fx_$sonance/,m=n=o=++X-1;o>=0;--op=o+d,q=_j(p*jF$q)*j_amt/512+.5,r=1;o<?r=o/:o>=+&&(r-=(o--)/X);s=i;j_1_f$q&&(s`qK1_xQv&&(s*=r*rKg`s;t=1(g)*1_vol;s=k,2_xQv&&(s*=r*rKh`s,t`2(h)*2_vol,noise_fader&&(t`(2*Mh.random()-1)*noise_fader*rKt*=r/;u=fx_f$q;j_fx_f$q&&(u*=qKu=1.5*Mh.sin(3.141592*u/aKm`u*n;v=l*(t-n)-m;switch(n`u*v,fx_filtercase 1:t=vZ2:t=mZ3:t=nZ4:t=m+v}if(s=e(p*panF$q)*fx_pan_amt/512+.5,t*=39*Qv_master,p=4*p,p+3<c.lQgthw=c[p]+(c[p+1+t*(1-s);c[p]=&w,c[p+1]=w,w=c[p+2]+(c[p+3+t*s,c[p+2]=&w,c[p+3]=w}}},=N,bc=++X-1+32*,d=this;k(c,N(ed.gQ,e,0Kl(e,c,d.instr,d.,N(b(O(e)EEE,c$e=N,b,Nb.()EE,c$e=N,b,Na.(bE)Rbsong=b,=a*bLQgQereTrack=N,d,ef=this;k(,N(gh=f.,i=f.*b*2,A=f.,k=f.QdPtern,m=O,AKn=o=p=qRfor(b=;;)if(32!==pif(o===k-1Hr,0);d=a.p[o];if(de=a.c[d-1].n[p];e&&m.gQ(e,g,nEif(n`A,p`1,-b>cHq	else p=o`1},rRl(g,h,a,A,tE,s=ti>s;f=d[s]+(d[s+1+g[s]+(g[s+1@;if(d[s]=&f,d[s+1]=f,s`2t	Ie	;Iq	)=Nb=this;k(,N(cd=eRd<bDa.lQgth?(d`1,b.gQereTrack(bDa[d-1],c,e)):a(O(c)E;e(E)c$e=N(N(ba(b.()E)c$e=N(N(bb.EE}();';G=/[-H-KX-Z#$QRNOEV@^`j]/.exec(_);)with(_.split(G))_=join(shift());eval(_)
-
-// song
-!function(){"use strict";window.song={songLen:119,songData:[{osc1_oct:10,osc1_det:0,osc1_detune:0,osc1_xenv:0,osc1_vol:59,osc1_waveform:0,osc2_oct:12,osc2_det:7,osc2_detune:0,osc2_xenv:0,osc2_vol:95,osc2_waveform:0,noise_fader:0,env_attack:0,env_sustain:0,env_release:47272,env_master:21,fx_filter:2,fx_freq:7800,fx_resonance:240,fx_delay_time:8,fx_delay_amt:34,fx_pan_freq:3,fx_pan_amt:0,lfo_osc1_freq:0,lfo_fx_freq:0,lfo_freq:0,lfo_amt:0,lfo_waveform:0,p:[1,1,1,1,1,2,0,0,0,0,2,1,1,2],c:[{n:[142,0,0,0,142,0,0,142,147,145,0,0,140,0,0,0,138,0,0,0,0,0,0,0,0,0,0,0,138,140,0,0]},{n:[135,0,0,0,135,0,0,135,140,138,0,0,137,0,0,0,130,0,0,0,134,0,0,0,142,0,0,0,126,128,0,0]}]},{osc1_oct:7,osc1_det:0,osc1_detune:0,osc1_xenv:0,osc1_vol:255,osc1_waveform:3,osc2_oct:8,osc2_det:0,osc2_detune:8,osc2_xenv:0,osc2_vol:74,osc2_waveform:3,noise_fader:0,env_attack:47272,env_sustain:40909,env_release:46363,env_master:255,fx_filter:2,fx_freq:224,fx_resonance:67,fx_delay_time:2,fx_delay_amt:25,fx_pan_freq:0,fx_pan_amt:0,lfo_osc1_freq:0,lfo_fx_freq:0,lfo_freq:0,lfo_amt:0,lfo_waveform:0,p:[0,0,0,1,1,2,3,2,3,2,2,1,1,2],c:[{n:[118,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,114,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},{n:[123,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,130,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},{n:[139,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,142,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}]},{osc1_oct:8,osc1_det:0,osc1_detune:0,osc1_xenv:0,osc1_vol:192,osc1_waveform:2,osc2_oct:8,osc2_det:7,osc2_detune:0,osc2_xenv:0,osc2_vol:172,osc2_waveform:2,noise_fader:0,env_attack:48181,env_sustain:80909,env_release:51818,env_master:210,fx_filter:2,fx_freq:1300,fx_resonance:43,fx_delay_time:6,fx_delay_amt:172,fx_pan_freq:1,fx_pan_amt:125,lfo_osc1_freq:0,lfo_fx_freq:1,lfo_freq:3,lfo_amt:180,lfo_waveform:0,p:[0,0,1,0,1,2,3,0,3,2,2,1,1,2,1,1,2,2,2,1],c:[{n:[118,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},{n:[123,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,130,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},{n:[139,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,145,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}]},{osc1_oct:6,osc1_det:0,osc1_detune:0,osc1_xenv:1,osc1_vol:193,osc1_waveform:0,osc2_oct:5,osc2_det:7,osc2_detune:0,osc2_xenv:0,osc2_vol:80,osc2_waveform:0,noise_fader:0,env_attack:50,env_sustain:0,env_release:17272,env_master:255,fx_filter:1,fx_freq:0,fx_resonance:254,fx_delay_time:0,fx_delay_amt:0,fx_pan_freq:0,fx_pan_amt:0,lfo_osc1_freq:0,lfo_fx_freq:0,lfo_freq:0,lfo_amt:0,lfo_waveform:3,p:[0,1,1,1,0,2,1,2,2,2,2,1,0,2,0,1,2,2,2],c:[{n:[142,0,0,0,0,0,0,142,0,142,0,0,0,0,140,0,138,0,0,0,0,0,0,138,0,138,0,0,0,140,0,0]},{n:[135,0,0,0,0,0,135,135,0,135,0,0,0,0,145,0,142,0,0,0,0,0,142,142,0,142,0,0,0,0,145,0]}]},{osc1_oct:8,osc1_det:0,osc1_detune:0,osc1_xenv:0,osc1_vol:200,osc1_waveform:0,osc2_oct:8,osc2_det:7,osc2_detune:8,osc2_xenv:0,osc2_vol:200,osc2_waveform:3,noise_fader:0,env_attack:36800,env_sustain:36800,env_release:36800,env_master:20,fx_filter:4,fx_freq:11025,fx_resonance:240,fx_delay_time:6,fx_delay_amt:100,fx_pan_freq:3,fx_pan_amt:254,lfo_osc1_freq:0,lfo_fx_freq:1,lfo_freq:6,lfo_amt:210,lfo_waveform:0,p:[0,0,0,0,1,0,1,0,0,2,2,1,0,2,0,1,2,2,2,1,1],c:[{n:[142,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},{n:[135,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,142,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}]},{osc1_oct:8,osc1_det:0,osc1_detune:0,osc1_xenv:0,osc1_vol:0,osc1_waveform:0,osc2_oct:8,osc2_det:0,osc2_detune:0,osc2_xenv:0,osc2_vol:0,osc2_waveform:0,noise_fader:148,env_attack:3636,env_sustain:4545,env_release:39090,env_master:31,fx_filter:2,fx_freq:3100,fx_resonance:122,fx_delay_time:5,fx_delay_amt:132,fx_pan_freq:0,fx_pan_amt:0,lfo_osc1_freq:0,lfo_fx_freq:1,lfo_freq:5,lfo_amt:147,lfo_waveform:0,p:[0,0,0,1,2,0,1,0,2,2,0,4,3,0,0,0,3,4],c:[{n:[142,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},{n:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,138,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},{n:[135,0,0,0,0,0,0,0,135,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,142,0,0,0,0,0,0,0]},{n:[142,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,138,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}]},{osc1_oct:7,osc1_det:0,osc1_detune:0,osc1_xenv:0,osc1_vol:187,osc1_waveform:2,osc2_oct:5,osc2_det:0,osc2_detune:2,osc2_xenv:1,osc2_vol:161,osc2_waveform:2,noise_fader:0,env_attack:100,env_sustain:1818,env_release:2727,env_master:60,fx_filter:1,fx_freq:1900,fx_resonance:162,fx_delay_time:2,fx_delay_amt:153,fx_pan_freq:6,fx_pan_amt:61,lfo_osc1_freq:0,lfo_fx_freq:1,lfo_freq:2,lfo_amt:196,lfo_waveform:3,p:[0,0,0,0,0,0,0,0,1,1,1,2,2,1],c:[{n:[135,135,138,135,140,138,137,135,135,138,135,140,138,135,142,0,142,142,145,142,147,145,144,142,142,145,142,147,145,142,0,0]},{n:[142,142,144,142,147,145,144,142,142,144,142,147,145,144,142,0,138,138,141,138,143,141,140,138,141,138,143,141,140,138,0,0]}]},{osc1_oct:8,osc1_det:0,osc1_detune:0,osc1_xenv:0,osc1_vol:0,osc1_waveform:0,osc2_oct:8,osc2_det:0,osc2_detune:0,osc2_xenv:0,osc2_vol:0,osc2_waveform:0,noise_fader:34,env_attack:50,env_sustain:2727,env_release:50,env_master:43,fx_filter:1,fx_freq:3224,fx_resonance:254,fx_delay_time:6,fx_delay_amt:36,fx_pan_freq:7,fx_pan_amt:254,lfo_osc1_freq:0,lfo_fx_freq:1,lfo_freq:5,lfo_amt:187,lfo_waveform:0,p:[0,0,0,0,0,0,0,0,0,1,1,2,2,1,1,1,2],c:[{n:[135,135,135,135,0,135,0,135,0,135,0,135,135,135,135,135,142,142,142,142,0,142,0,142,0,142,0,142,142,142,0,142]},{n:[142,142,142,142,0,142,0,142,0,142,0,142,142,142,142,142,138,138,138,138,0,138,0,138,0,138,0,138,138,138,0,138]}]}],rowLen:7350,endPattern:22}}();
-
-;(function() {
-    var audioCtx = new AudioContext();
-    var source;
-    var songGen = new sonantx.MusicGenerator(song);
-    songGen.createAudioBuffer(function(buffer) {
-        source = audioCtx.createBufferSource(); // Create Sound Source
-        source.buffer = buffer; // Add Buffered Data to Object
-        source.connect(audioCtx.destination); // Connect Sound Source to Output
-        audioCtx.isPlaying = true;
-        source.loop = true;
-        source.start();
-    });
-/*
-    console.log(audioCtx.isPlaying);
-    if (audioCtx.isPlaying) {
-        audioCtx.suspend();
-        audioCtx.isPlaying = false;
-    } else {
-        audioCtx.resume();
-        audioCtx.isPlaying = true;
-    }
-*/
-    window.audioCtx = audioCtx;
-})();
-
 ;(function() {
     var resourceCache = {};
     var loading = [];
@@ -691,7 +660,7 @@ for(_='lfoj+=`3276^-^8@d=@>d?@:d>^7?^7:d,V)}Enew OfunctionN=N(RenQre$=String.fro
         switch (tutCnt) {
             case 1:
                 tutoTxt1 = "Ship player may use LEFT/RIGHT arrows to move.";
-                tutoTxt2 = "Press 'M' to toggle MUSIC on/off.";
+                tutoTxt2 = "";
                 break;
             case 60:
                 movingLeft = true;
@@ -789,19 +758,6 @@ for(_='lfoj+=`3276^-^8@d=@>d?@:d>^7?^7:d,V)}Enew OfunctionN=N(RenQre$=String.fro
         if ((!ship.isSinking && role == "sub") || demoMode) {
             if (movingLeft) ship.pos[0] -= shipSpeed * dt;
             if (movingRight) ship.pos[0] += shipSpeed * dt;
-        }
-        if (input.isDown("m") && !audioCtx.isChanging) {
-            audioCtx.isChanging = true;
-            if (audioCtx.isPlaying) {
-                audioCtx.suspend();
-                audioCtx.isPlaying = false;
-            } else {
-                audioCtx.resume();
-                audioCtx.isPlaying = true;
-            }
-            setTimeout(function() {
-                audioCtx.isChanging = false;
-            }.bind(this), 500);
         }
         if (demoMode) return;
 
